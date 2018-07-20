74cb5dcc784388a7d0d9c86469452ad83647da1b
Michael Nwabuobi <40067128+IamMicco@users.noreply.github.com>
2018-07-19 19:57:41 -0500

    Update gui_app.py

--- gui_app.py
+++ gui_app.py
@@ -17,7 +17,7 @@ class Application:
 
         file = Menu(mainmenu, tearoff = 0)
         mainmenu.add_cascade(label = 'File', menu = file)
-        file.add_command(label = 'New', command = self.create_user1)
+        file.add_command(label = 'New', command = self.create_user)
         file.add_separator()
         file.add_command(label = 'Exit', command = root.quit)
 
@@ -35,7 +35,7 @@ class Application:
         frame2.pack()
 
         view_button = Button(frame2, text = 'VIEW', fg = 'blue', command = self.submit_view_user).grid(row = 1, column = 1, sticky = W)
-        delete_button = Button(frame2, text = 'delete', fg = 'red', command = delete_customer).grid(row = 1, column = 2, sticky = E)
+        delete_button = Button(frame2, text = 'delete', fg = 'red', command = self.delete_user).grid(row = 1, column = 2, sticky = E)
 
 
 
@@ -51,13 +51,16 @@ class Application:
     def submit_view_user(self):
         self.text.insert(END, f'{app.view_customer(self.email1.get())}')
 
-    def create_user1(self):
+    def create_user(self):
         top = Toplevel(self.master)
-        data = Top(top)
+        data = Create(top)
+
+    def delete_user(self):
+        top = Toplevel(self.master)
+        data = Delete(top)
 
 
-
-class Top:
+class Create:
         
     def __init__(self, master):
         self.firstname = StringVar()
@@ -67,22 +70,21 @@ class Top:
             
 
         self.master = master
-        # self.master.geometry('500X200')
         self.master.title('Create User')
 
-        self.first_name = Label(self.master, text ="Firstname", font=('Times', 15)).grid(row=0,column=0, sticky=W)
+        self.first_name = Label(self.master, text ="Firstname").grid(row=0,column=0, sticky=W)
         self.entry1 = Entry(self.master, textvariable = self.firstname).grid(row = 0, column = 1)
 
-        self.last_name = Label(self.master, text ="Lastname", font=('Times', 15)).grid(row=1,column=0, sticky=W)
+        self.last_name = Label(self.master, text ="Lastname").grid(row=1,column=0, sticky=W)
         self.entry2 = Entry(self.master, textvariable = self.lastname).grid(row = 1, column = 1)
 
-        self.email = Label(self.master, text ="Email", font=('Times', 15)).grid(row=2,column=0, sticky=W)
+        self.email = Label(self.master, text ="Email").grid(row=2,column=0, sticky=W)
         self.entry3 = Entry(self.master, textvariable = self.email_).grid(row = 2, column = 1)
 
-        self.phone_number = Label(self.master, text = 'Phone number', font = ('Times', 15)).grid(row = 3, column = 0, sticky = W)
+        self.phone_number = Label(self.master, text = 'Phone number').grid(row = 3, column = 0, sticky = W)
         self.entry4 = Entry(self.master, textvariable = self.phone).grid(row = 3, column = 1, sticky = W)
 
-        self.button1 = Button(self.master, text = 'submit', command = submit_new_user).grid(row = 4, column = 1, columnspan = 2, sticky = W)
+        self.button1 = Button(self.master, text = 'submit', command = submit_new_user).grid(row = 6, column = 3, columnspan = 2, sticky = W)
         self.button1.pack()
 
 
@@ -91,20 +93,20 @@ class Top:
             app.add_customer(name, self.email_.get(), self.phone.get())
 
 
+class Delete:
+    
+    def __init__(self, master):
+        self.email_ = StringVar()
+    
+        self.master = master
+        self.master.title('Delete User')
 
+        self.email = Label(self.master, text ="Email").grid(row=2,column=0, sticky=W)
+        self.entry2 = Entry(self.master, textvariable = self.email_).grid(row = 2, column = 1)
 
-def delete_customer():
-    view = Toplevel()
-    view.title("Delete user")
+        self.submit = Button(self.master, text = 'submit', command = self.submit_delete_user)
 
-    email = Label(view, text ="Email", font=('Times', 15))
-    email.grid(row=2,column=0, sticky=W)
-    email_ = StringVar()
-    entry2 = Entry(view, textvariable = email_).grid(row = 2, column = 1)
-
-    submit = Button(view, text = 'submit', command = submit_delete_user)
-
-    def submit_delete_user():
+    def submit_delete_user(self):
         confirm_delete = Toplevel()
         confirm_delete.title("Confirm Delete user")
 
@@ -113,8 +115,7 @@ def delete_customer():
         cancel_button = Button(confirm_delete, text = 'cancel', command = confirm_delete.destroy).grid(row = 1, column = 2, sticky = W)
 
         def conf_delete():
-            app.delete_customer(email_.get())
-
+            app.delete_customer(self.email_.get())
     
 
 

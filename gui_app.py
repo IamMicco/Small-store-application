a083acd99ee55da00cfd1b7e8392eb6c7159be60
Michael Nwabuobi <40067128+IamMicco@users.noreply.github.com>
2018-07-22 17:51:32 -0500

    Added new features

--- gui_app.py
+++ gui_app.py
@@ -23,6 +23,7 @@ class Application:
         file = Menu(mainmenu, tearoff = 0)
         mainmenu.add_cascade(label = 'File', menu = file)
         file.add_command(label = 'New', command = self.create_user)
+        file.add_command(label = 'Current', command = self.update_current_user)
         file.add_separator()
         file.add_command(label = 'Delete', command = self.delete_user)
         file.add_command(label = 'Exit', command = root.quit)
@@ -62,6 +63,7 @@ class Application:
 
 
     def submit_view_user(self):
+        self.text.delete(1.0,END)
         for item in app.view_customer(self.email.get()):
             self.text.insert(END, item)
 
@@ -69,6 +71,10 @@ class Application:
     def create_user(self):
         top = Toplevel(self.master)
         data = Create(top)
+
+    def update_current_user(self):
+        top = Toplevel(self.master)
+        data = Update(top)
 
     def delete_user(self):
         top = Toplevel(self.master)
@@ -115,7 +121,82 @@ class Create:
     def submit_new_user(self):
         name = self.firstname.get() + ' ' + self.lastname.get()
         app.add_customer(name, self.email_.get(), self.phone.get())
+        top = Toplevel(self.master)
+        data = Create_user_items(top)
         root.quit()
+
+class Create_user_items:
+    
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Add Items')
+
+        self.item_button1 = Button(self.master, text = 'Item1', command = self.add_item1)
+        self.item_button1 = Button(self.master, text = 'Item2', command = self.add_item2)
+        self.item_button1 = Button(self.master, text = 'Item3', command = self.add_item3)
+        self.item_button1 = Button(self.master, text = 'Item4', command = self.add_item4)
+        self.item_button1 = Button(self.master, text = 'Item5', command = self.add_item5)
+        self.item_button1 = Button(self.master, text = 'Item6', command = self.add_item6)
+        self.item_button1.pack()
+        self.item_button1.pack()
+        self.item_button1.pack()
+        self.item_button1.pack()
+        self.item_button1.pack()
+        self.item_button1.pack()
+
+    def add_item1(self):
+        pass
+
+    def add_item2(self):
+        pass
+
+    def add_item3(self):
+        pass
+
+    def add_item4(self):
+        pass
+
+    def add_item5(self):
+        pass
+
+    def add_item6(self):
+        pass
+
+
+
+class Update:
+    
+    def __init__(self, master):
+        self.email_ = StringVar()
+
+        self.master = master
+        self.master.title('Find Customer')
+
+        self.email = Label(self.master, text ="Email")
+        self.entry = Entry(self.master, textvariable = self.email_)
+        self.button = Button(self.master, text = 'Find User', command = self.find_user)
+        self.email.pack()
+        self.entry.pack()
+        self.button.pack()
+
+    def find_user(self):
+        return app.find_customer(self.email_)
+
+    def update_information(self):
+        top = Toplevel(self.master)
+        data = Update_info(top, self.find_user())
+
+class Update_info:
+    
+    def __init__(self, master, customer):
+        self.customer = customer
+
+        self.master = master
+        self.master.title('Update Customer Items')
+
+        # In-complete
+
+        
 
 
 class Delete_data:
@@ -138,6 +219,7 @@ class Delete_data:
     def confirm_delete_user(self):
         top = Toplevel(self.master)
         data = Confirm(top, self.email)
+
 
 class Confirm:
     def __init__(self, master, email):

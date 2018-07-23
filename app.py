b806898e68924f76afebaa3e0fc35042e6f277c8
Michael Nwabuobi <40067128+IamMicco@users.noreply.github.com>
2018-07-23 15:49:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from collections import OrderedDict
 from datetime import datetime
 
 
-
 db = SqliteDatabase('garment.db')
 
 class BaseModel(Model):
@@ -29,45 +28,22 @@ def initialize():
     db.create_tables([Customer, Garment], safe=True)
 
 
-def menu_loop():
-    choice = None
-    while choice != 'q':
-        print ('Make a choice')
-        for key, value in menu.items():
-            print (f'{key}), {value.__doc__}')
-
-        choice = input('Action: ').lower().strip()
-        if choice in menu:
-            if menu[choice] == view_customer or menu[choice] == delete_customer:
-                email = input('Enter email: ')
-                menu[choice](email)
-            else:
-                menu[choice]()
-
-
-# add_customer function needs editing
-
 def add_customer(name = None, email = None, phone = None):
     '''Add Customer'''
 
     customer = Customer.create(name = name, email=email, phone_number = phone)
-    action = None
-    while action != 'yes':
-        customer_purchase = input('Enter products color: ').strip()
-        Garment.create(customer=customer, color=customer_purchase)
-        action = input('Are you done? yes/no').lower().strip()
+   
 
-def current_customer(email=None):
+def add_customer_items(customer = None, color = None):
+    if customer != None and color != None:
+        Garment.create(customer = customer, color = color)
+
+def current_customer(customer = None, color = None):
     '''Add to current Customer'''
 
-    if email != None:
-        customer = Customer.get(Customer.email==email)
-        action = None
-
-        while action != 'yes':
-            customer_purchase = input('Enter products color: ').strip()
-            Garment.create(customer=customer, color=customer_purchase)
-            action = input('Are you done? yes/no').lower().strip()
+    if customer != None and color != None:
+        Garment.create(customer = customer, color = color)
+  
 
 def find_customer(email = None):
     
@@ -94,16 +70,7 @@ def delete_customer(email=None):
     if customer:
         customer.delete_instance()
         
-        
 
-menu = OrderedDict([
-    ('a', add_customer),
-    ('v', view_customer),
-    ('d', delete_customer),
-    ('c', current_customer),
-])
 
 if __name__ == '__main__':
     initialize()
-    menu_loop()
-    # print (find_customer('abc@gmail.com'))
